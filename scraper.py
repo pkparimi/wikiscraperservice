1aa5b81fe9c1a8a46ec79b8a0f9dfd6214d6178a
Pavan Parimi <parimip@flip3.engr.oregonstate.edu>
2021-11-02 11:44:56 -0700

    basis for full scope

--- scraper.py
+++ scraper.py
@@ -12,12 +12,31 @@ article_dict = {}
 
 toc = soup.find(id="toc")
 
+# description section
 p_before_toc = toc.find_all_previous("p")
 
 description = ""
 
 for p in p_before_toc:
-    if not p.text.isspace():
-        description = description + p.text + "\r\n"
+    if not p.text.isspace():  # to avoid taking blank paragraphs
+        for sup in p("sup"):  # clears all wikipedia annotations
+            sup.decompose()
+        description = p.text + "\r\n" + description
 
-print(description)
+article_dict["description"] = description
+
+thissoup = toc.find_all_next(string="Etymology")[1]
+
+nextsoup = toc.find_all_next(string="History")[1]
+
+p_after = thissoup.find_all_next("p")
+p_before = nextsoup.find_all_previous("p")
+
+p_total = []
+
+for p in p_after:
+    if p in p_before:
+        p_total.append(p)
+        
+
+print("done")
