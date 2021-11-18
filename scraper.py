4a95e7295a88018af57e3746bc8f7e0dfe747c3a
pkparimi <pavanparimi92@gmail.com>
2021-11-18 12:03:12 -0800

    scraper and wikiscraper_web refactored

--- scraper.py
+++ scraper.py
@@ -4,18 +4,34 @@ import locale
 
 
 class Scraper:
-    def __init__(self, article) -> None:
+    """
+    This is a class to scrape articles from wikipedia.
+    
+    Attributes:
+        article (string) : Name of article to scrape
+        article_dict (dictionary) : Article scraped in dictionary format
+        article_list (list) : Article scraped in list format
+        soup (BeautifulSoup object) : Beautiful soup object of scraped article
+    """
+    
+    def __init__(self, article: str) -> None:
+        """
+        The constructor for a Scraper object
+        """
         self.article = article
         self.article_dict = {}
         self.article_list = []
 
+        # parses html of wikipedia article into a BeautifulSoup Object
         URL = f'https://en.wikipedia.org/wiki/{self.article}'
-        
         resp = requests.get(URL)
-        
-        self.soup = BeautifulSoup(resp.text, 'html.parser')
+        self.soup = BeautifulSoup(resp.text, 'html.parser') 
     
-    def table_of_content_creator(self):
+    def table_of_content_creator(self) -> None:
+        """
+        Creates table of contents as keys in article dict and as list
+        """
+        
         toc_soup = self.soup.find(id="toc")
         breaker = False
         level = 1
@@ -27,6 +43,7 @@ class Scraper:
             cur_bullet = toc_soup.select('li[class*="tocsection-' + str(section) + '"]')[0]  # gets current TOC list item tag object
             section_text = cur_bullet.select(".toctext")[0].text # gets text of TOC list item
             
+            # breaks loop, end of article
             if section_text in ("See also", "See Also", "Notes", "References"):
                 self.article_list.append(section_text)
                 break
@@ -72,8 +89,11 @@ class Scraper:
             section += 1
         return self.article_dict, self.article_list
     
-    def toc_find_by_key(self, value, dic):
-        # iterates through nested dictionary to find key 
+    def toc_find_by_key(self, value: str, dic: dict) -> object:
+        """
+        Iterates through nested dictionary to find key for table of contents.
+        """
+        
         if value in dic:
             return dic[value]
         for v in dic.values():
@@ -82,8 +102,11 @@ class Scraper:
                     return self.toc_find_by_key(value, v)
         return None
     
-    def dict_find_by_key(self, value, dic):
-        # iterates through nested dictionary to find key 
+    def dict_find_by_key(self, value: str, dic: dict) -> object:
+        """
+        Iterates through nested dictionary to find key for full dictionary text.
+        """
+        
         if value in dic:
             if dic[value] is None:
                 dic[value] = {}
@@ -94,7 +117,10 @@ class Scraper:
                 if nested_dict is not None:
                     return nested_dict
                 
-    def article_text_retriever(self):
+    def article_text_retriever(self) -> None:
+        """
+        Retrieves actual article text of each article in TOC
+        """
         for i in range(len(self.article_list)-1):
             description = ""
             p_total = []
@@ -132,11 +158,17 @@ class Scraper:
             this_dict["info"] = description
             print("-processing " + self.article_list[i])
 
-    def get_basic_description(self):
+    def get_basic_description(self) -> None:
+        """
+        Gets basic description of wikipedia article (text before the Table of Contents).
+        """
+        
+        # determine location of table of contents
         toc_soup = self.soup.find(id="toc")
         p_before_toc = toc_soup.find_all_previous("p")
+        
+        # parse all paragraphs before table of contents
         pre_toc_description = ""
-
         for p in p_before_toc:
             if not p.text.isspace():  # to avoid taking blank paragraphs
                 for sup in p("sup"):  # clears all wikipedia annotations
@@ -146,7 +178,7 @@ class Scraper:
         self.article_dict["info"] = pre_toc_description
         
     
-    def get_images(self, format):
+    def get_images(self, format: str) -> None:
         if format == "dictionary":
             self.article_dict["images"] = {}
             image_dict = self.article_dict["images"]
@@ -185,10 +217,17 @@ class Scraper:
                     image_list.append(image['src'])
 
             
-    def get_capital(self):
+    def get_capital(self) -> None:
+        """
+        Gets capital values of country data.
+        """
+        
         try:
+            # parsed text of capital from beautiful soup object
             capital = self.soup.find('th',text="Capital")
             capitalname = capital.next_sibling.text
+            
+            # parses capital text for capital name and gps coordinates
             capital_name_breakpoint = None
             capital_gps_breakpoint = None
             for i in range(len(capitalname)):
@@ -203,17 +242,28 @@ class Scraper:
             self.article_dict["capital_name"] = "NOT FOUND IN THIS ARTICLE"
             self.article_dict["capital_gps"] = "NOT FOUND IN THIS ARTICLE"
             
-    def get_language(self):
+    def get_language(self) -> None:
+        """
+        Gets language value of country data.
+        """
+        
         try:
-            official_languages = self.soup.find(name='th', text='Official\xa0languages')
-            self.article_dict["language_name"] = official_languages.next_sibling.find('a').text
+            official_languages = self.soup.find(name='th', text='Official\xa0languages').next_sibling.find('a').text  # parsed text of language from beautiful soup object
+            self.article_dict["language_name"] = official_languages
         except:
             self.article_dict["language_name"] = "NOT FOUND IN THIS ARTICLE"
             
-    def get_population(self):
+    def get_population(self) -> None:
+        """
+        Gets population values of country data.
+        """
+        
         try:
+            # parsed text of population from beautiful soup object
             population = self.soup.find('a', text='Population').parent.parent.nextSibling.find(class_='infobox-data').text
             population = population.strip()
+            
+            # formats population text in correct format
             for i in range(len(population)):
                 if not population[i].isdigit() and population[i] != ',':
                     self.article_dict["population"] = population[:i]
@@ -221,10 +271,17 @@ class Scraper:
         except:
             self.article_dict["population"] = "NOT FOUND IN THIS ARTICLE"
     
-    def get_GDP(self):
+    def get_GDP(self) -> None:
+        """
+        Gets GDP values of country data.
+        """
+        
         try:
+            # parsed text of GDP from beautiful soup object
             GDP_total = self.soup.find('a', text='GDP').parent.parent.nextSibling.find(class_='infobox-data').text
             GDP_total = GDP_total.strip()
+            
+            # formats GDP text in correct format for GDP
             for i in range(len(GDP_total)):
                 if GDP_total[i] ==  'n':
                     self.article_dict["GDP_total"] = GDP_total[:i+1]
@@ -242,32 +299,34 @@ class Scraper:
         except:
             self.article_dict["GDP_per_cap"] = "NOT FOUND IN THIS ARTICLE"
             
-    def get_area(self):
+    def get_area(self) -> None:
+        """
+        Gets area values of country data.
+        """
+        
         try:
+            # parsed text of area from beautiful soup object
             area = self.soup.find('a', text='Area ').parent.parent.nextSibling.find(class_='infobox-data').text
             area = area.strip()
-            locale.setlocale(locale.LC_ALL, 'en_US')
+            
+            # formats area into correct format for km and mi respectively 
             for i in range(len(area)):
-                if area[i] == '\xa0':
+                if area[i] == '\xa0':  # when \xa0 is encountered, this is a space and the end of the number part of the km^2 string
                     self.article_dict["area_km"] = area[:i] + " km^2"
-                    self.article_dict["area_mi"] = locale.format("%d", int(int(area[:i].replace(",","")) * 0.386102), grouping=True) + " mi^2"
+                    locale.setlocale(locale.LC_ALL, 'en_US')
+                    self.article_dict["area_mi"] = locale.format("%d", int(int(area[:i].replace(",","")) * 0.386102), grouping=True) + " mi^2"  # convert km^2 to mi^2
                     break
         except:
             self.article_dict["area_km"] = "NOT FOUND IN THIS ARTICLE"
             self.article_dict["area_mi"] = "NOT FOUND IN THIS ARTICLE"
     
-    def get_currency(self):
+    def get_currency(self) -> None:
+        """
+        Gets currency values of country data.
+        """
         try:
-            currency = self.soup.find('th', text='Currency').nextSibling.text
+            currency = self.soup.find('th', text='Currency').nextSibling.text  # parsed text of currency from beautiful soup object
             self.article_dict["currency"] = currency
         except:
             self.article_dict["currency"] = "NOT FOUND IN THIS ARTICLE"
         
-if __name__ == "__main__":
-    this_scraper = Scraper("United_States")
-    
-    this_scraper.table_of_content_creator()
-        
-    this_scraper.article_text_retriever()
-    
-    print(this_scraper.article_dict)
--- wikiscraper_web.py
+++ wikiscraper_web.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import json
+from json import dumps
 from flask_cors import CORS
 from scraper import Scraper
 
@@ -11,95 +11,150 @@ CORS(app)  # allows for cross-origin requests
 # Route
 @app.route("/", methods=['GET'])
 def scraper():
+    """
+    get:
+        summary: Scrapes the wikipedia areticle and serves a JSON via a get request.
+        parameters:
+            -article: article name of wikipedia article
+            -full_text: y if full text is wanted, n if only description, default is n
+            -images: y if images are requires, n if not, default is n
+            -image_format: dict for dictionary, list for list, main for just main images, default is main
+        responses:
+            200:
+                if article parameters are valid:
+                    content: application/json 
+                else:
+                    html text of error
+    """
+    
+    # get arguments
     article, full_text, images, image_format, country_data = get_args(request)
 
+    # proceeds through scraping cascade if article param is given
     if article:
-        return get_article(article, full_text, images, image_format, country_data)
-        try:
-            this_scraper = Scraper(article)
-        except:
+        this_scraper = get_article(article)
+        if not this_scraper:  # if scraper object returns error when searching for article
             return "<h1>Sorry that article was not found!<h1>"
         
-        if full_text == "y":
-            try:
-                this_scraper.table_of_content_creator()
-                this_scraper.article_text_retriever()
-                this_scraper.get_basic_description()
-            except:
-                return "<h1>Sorry there was an error retrieving the article text<h1>"
-        else:
-            try:
-                this_scraper.get_basic_description()
-            except:
-                return "<h1>Sorry there was an error retrieving the article text<h1>"
-            
-        if images == "y":
-            try:
-                if image_format == 'list':
-                    this_scraper.get_images(format="list")
-                elif image_format == "dictionary":
-                    this_scraper.get_images(format="dictionary")
-                else:
-                    this_scraper.get_images(format="main")
-            except:
-                return "<h1>Sorry there was an error retrieving the images, try different parameters.<h1>"
+        this_scraper = get_text(this_scraper, full_text)
+        if not this_scraper:  # if scraper object returns error when searching for article text
+            return "<h1>Sorry there was an error retrieving the article text<h1>"
+
+        this_scraper = get_images(this_scraper, image_format, images)
+        if not this_scraper:  # if scraper object returns error when searching for article images
+            return "<h1>Sorry there was an error retrieving the images, try different parameters.<h1>"
         
-        if country_data == "y":
+        this_scraper = get_country_data(this_scraper, country_data)
+        if not this_scraper:  # if scraper object returns error when searching for article country data
+            return "<h1>Sorry there was an error retrieving the country data, try different parameters.<h1>"
+        
+        # format response
+        response = app.response_class(
+            response=replace_strange_chars(dumps(this_scraper.article_dict)),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    
+    # if no article given in params
+    return 'Please enter article in query parameters'
+
+def replace_strange_chars(str_to_replace: str) -> str:
+    """
+    Replaces misparsed UTF characters with equivalents.
+    """
+    
+    replacements = {'\\n':' ', 
+                    '\\u00a0' : ' ',
+                    '\\u2013' : '-',
+                    '\\u2014' : '-',
+                    '\\u00f3' : 'o',
+                    '\\u00fc' : 'u',
+                    '\\u00e9' : 'e',
+                    '\\ufeff' : '',
+                    '\\u00b0' : 'degrees'
+                }
+    
+    for key, value in replacements.items():
+        str_to_replace = str_to_replace.replace(key, value)
+
+    return str_to_replace
+    
+def get_country_data(this_scraper: object, country_data: str) -> object:
+    """
+    Performs country data scrape for current scraper.
+    """
+    
+    if country_data == "y":
+        try:
             this_scraper.get_area()
             this_scraper.get_capital()
             this_scraper.get_GDP()
             this_scraper.get_population()
             this_scraper.get_language()
             this_scraper.get_currency()
+        except:
+            return None
         
-        future_response = json.dumps(this_scraper.article_dict)
-        future_response = future_response.replace('\\n', ' ')
-        future_response = future_response.replace('\\u00a0', ' ')
-        future_response = future_response.replace('\\u2013', '-')
-        future_response = future_response.replace('\\u2014', '-')
-        future_response = future_response.replace('\\u00f3', 'o')
-        future_response = future_response.replace('\\u00fc', 'u')
-        future_response = future_response.replace('\\u00e9', 'e')
-        future_response = future_response.replace('\\ufeff', '')
-        future_response = future_response.replace('\\u00b0', 'degrees')
+    return this_scraper
+            
         
-        response = app.response_class(
-            response=future_response,
-            status=200,
-            mimetype='application/json'
-        )
-        return response
-    
-    return 'Please enter article in query parameters'
 
-def get_article(article, full_text, images, image_format, country_data):
+def get_article(article: str) -> object:
+    """
+    Creates article scraper object given article title text.
+    """
+    
     try:
         this_scraper = Scraper(article)
     except:
-        return "<h1>Sorry that article was not found!<h1>"
+        return None
     
-    if not get_text(this_scraper, full_text):
-        return "<h1>Sorry there was an error retrieving the article text<h1>"
+    return this_scraper
     
-def get_text(scraper, full_text):
+def get_text(this_scraper: object, full_text: str) -> object:
+    """
+    Performs text scrape for current scraper.
+    """
     
     if full_text == 'y':
         try:
-            scraper.table_of_content_creator()
-            scraper.article_text_retriever()
-            scraper.get_basic_description()
+            this_scraper.table_of_content_creator()
+            this_scraper.article_text_retriever()
+            this_scraper.get_basic_description()
         except:
-            return False
+            return None
     else:
         try:
-            scraper.get_basic_description()
+            this_scraper.get_basic_description()
         except:
-            return False
+            return None
         
-    return True
+    return this_scraper
+
+def get_images(this_scraper: object, image_format: str, images: str) -> object:
+    """
+    Performs image scrape for current scraper.
+    """
     
+    if images == "y":
+        try:
+            if image_format == 'list':
+                this_scraper.get_images(format="list")
+            elif image_format == "dictionary":
+                this_scraper.get_images(format="dictionary")
+            else:
+                this_scraper.get_images(format="main")
+        except:
+            return None
+        
+    return this_scraper
+     
+def get_args(request: object) -> tuple:
+    """
+    Retrieves arguments from get request.
+    """
     
-def get_args(request):
     article = request.args.get("article")
     full_text = request.args.get("full_text")
     images = request.args.get("images")
